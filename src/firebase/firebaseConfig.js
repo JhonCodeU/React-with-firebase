23ec2af2db18ad0260529001087c64fb4a88d875
JhonCode <jhon.380172793@ucaldas.edu.co>
2023-02-17 21:17:16 -0500

    listContact done

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,8 +1,9 @@
-/* // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://support.google.com/firebase/answer/7015592
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -13,29 +14,6 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-const db = getFirestore();
-
-export default db; */
-
-
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://support.google.com/firebase/answer/7015592
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCGbs98pAm0sUnHbM9PckYdkhvgvchnaP0",
-  authDomain: "vue-http-demo-4b483.firebaseapp.com",
-  databaseURL: "https://vue-http-demo-4b483-default-rtdb.firebaseio.com",
-  projectId: "vue-http-demo-4b483",
-  storageBucket: "vue-http-demo-4b483.appspot.com",
-  messagingSenderId: "137399950730",
-  appId: "1:137399950730:web:eca9440224dffa197bbc73"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
